e1571df88e489465b3e0062f9025d7acd537518f
mikadamsterdam <mikadamsterdam@concepts.nl>
2016-02-11 11:03:18 +0100

    update without syntax issue

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,23 +6,21 @@ class SongsController < ApplicationController
 
 
   
-=begin
+
   def new
     @song = Song.new
   end
 
 
   def create
-   if @song = Song.create(user_params)
+   if @song = Song.create(song_params)
       redirect_to root_path
     else
       render :new
     end
   end
 
-  def show
-    	@song = Song.find(params[:id])
-  	end
+ 
 
 
   private
@@ -30,5 +28,6 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :release_date, :album, :artist_id)
   end
+
 end
-=end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,41 +10,37 @@
 #index is for the array
 #key is fir the hash
 
-marley = Artist.create
-[{ name: "Bob",
-  birth_date: Date.parse("1945-05-05")
-  }]
+#List of artists
+marley = Artist.create({name: "Bob",
+  birth_date: Date.parse("1945-05-05")}
+  )
 
-rihanna = Artist.create
-[{
-  name: "Rihanna",
+rihanna = Artist.create({name: "Rihanna",
   birth_date: Date.parse("1988-05-16")
-  }]
-
-mj = Artist.create
-[{
-name: "Mikael",
+  })
+mj = Artist.create({
+name: "Mikael Jackson",
 birth_date: Date.parse("1958-09-29")
-  }]
+})
 
 
 
-
-
-i_shot_the_sheriff = Song.create
-[{
+#list of songs
+i_shot_the_sheriff = Song.create({
   title: "I shot the sheriff",
   release_date: Date.parse("1970-05-05"),
-  album: "Burnin", artist_id: marley.id
-  }]
+  album: "Burnin", 
+  artist_id: marley.id
+  })
 
-thriller = Song.create
-[{
+thriller = Song.create({
   title: "Thriller",
   release_date: Date.parse("1982-11-30"),
-  album: "Burnin", artist_id: mj.id
-  }]
+  album: "Thriller", artist_id: mj.id
+  })
 
-bad = Song.create[{ title: "Bad", release_date: Date.parse( "1987-08-31"), album: "Bad", artist_id: mj.id }]
-man_down = Song.create[{ title: "Man down", release_date: Date.parse( "2010-01-01"), album: "Loud", artist_id: rihanna.id }]
-three_little_birds = Song.create[{ title: "three little birds", release_date: Date.parse( "1977-01-01"), album: "Exodus", artist_id: marley.id}]
+bad = Song.create({title: "Bad", release_date: Date.parse( "1987-08-31"), album: "Bad", artist_id: mj.id })
+
+man_down = Song.create({ title: "Man down", release_date: Date.parse( "2010-01-01"), album: "Loud", artist_id: rihanna.id })
+
+three_little_birds = Song.create({ title: "three little birds", release_date: Date.parse( "1977-01-01"), album: "Exodus", artist_id: marley.id})
